39df6002a52e5499437c14d62feeb76c6bcdaf95
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-11-07 22:45:13 +0100

    fixed build errors

--- src/constants/config/index.tsx
+++ src/constants/config/index.tsx
@@ -10,12 +10,6 @@ import { ETH_CHAINS } from "@/utils/network";
 
 export const WALLETCONNECT_PROJECT_ID = "b10ff2f14f1c8a4efecf95865ebb1ac2";
 
-if (!WALLETCONNECT_PROJECT_ID) {
-  console.warn(
-    "You need to provide a NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID env variable"
-  );
-}
-
 export const metadata = {
   name: SITE_NAME,
   description: SITE_DESCRIPTION,
@@ -28,12 +22,6 @@ export const metadata = {
 import { cookieStorage, createStorage, http } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 
-// Get projectId from https://dashboard.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
-
-if (!projectId) {
-	throw new Error("Project ID is not defined");
-}
 
 export const networks = ETH_CHAINS;
 
@@ -43,7 +31,7 @@ export const wagmiAdapter = new WagmiAdapter({
 		storage: cookieStorage,
 	}),
 	ssr: true,
-	projectId,
+	projectId: WALLETCONNECT_PROJECT_ID,
 	networks,
 });
 
